1a5082b2f0ce0da8c44461988f6bd39856c2de71
dip-patel-second <220303238+dip-patel-second@users.noreply.github.com>
2025-11-19 19:40:43 +0530

    email service updated

--- src/main/java/com/example/jobportal/Job_Portal_System/Service/UserService.java
+++ src/main/java/com/example/jobportal/Job_Portal_System/Service/UserService.java
@@ -35,7 +35,7 @@ public class UserService {
             User user = new User();
             user.setUserName(userDto.getUserName());
             user.setEmail(userDto.getEmail());
-            user.setPassword(userDto.getPassword());
+            user.setPassword(passwordEncoder.encode(userDto.getPassword()));
             user.setRoles(Arrays.asList("CANDIDATE"));
             userRepository.save(user);
             return true;
@@ -57,7 +57,8 @@ public class UserService {
         User user = new User();
         user.setUserName(userDto.getUserName());
         user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
         user.setRoles(Arrays.asList("CANDIDATE"));
 
         User savedUser = userRepository.save(user);
